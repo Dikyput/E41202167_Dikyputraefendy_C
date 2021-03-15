5ad6d15da433c50a40eb854b306026a6f215475a
Diky Putra <joykey.grapichdiky@gmail.com>
2021-03-16 02:54:05 +0700

    Create Latihan4.java

---
+++ Minggu2/Latihan4.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan4;
+import java.util.Scanner;
+/**
+ *
+ * @author DIKYPUT PC
+ */
+public class Latihan4 {
+public class prak_Scanner {
+}
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+   //buat scanner
+        Scanner s1 = new Scanner (System.in);
+        
+        //Ambil nilai dari keyboard
+        System.out.print("Masukkan Nilai 1 = ");
+        int nilai1 = s1.nextInt();
+        System.out.print("Masukkan Nilai 2 = ");
+        int nilai2 = s1.nextInt();
+        
+        //Hitung dan Print/cetak
+        int jumlah = nilai1 + nilai2;
+        System.out.println("Jumlah = " + jumlah);
+    }
+    
+}
