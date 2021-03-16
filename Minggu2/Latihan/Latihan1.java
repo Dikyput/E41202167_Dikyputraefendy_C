c94f8c3d90e91aa99a9f7217d8d43bf892ffb138
Diky Putra <joykey.grapichdiky@gmail.com>
2021-03-16 10:27:09 +0700

    Create Latihan1.java

---
+++ Minggu2/Latihan/Latihan1.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan1;
+
+/**
+ *
+ * @author DIKYPUT PC
+ */
+public class Latihan1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+     System.out.println("Hello ");//Memanggil Output Kata Helo kemudian baris baru
+     System.out.println("World !");//Memanggil Output Kata World ! kemudian baris baru
+     
+     System.out.print("Hello ");//Memanggil Output Kata Helo 
+     System.out.print("World !");//Memanggil Output Kata World !
+    }
+    
+}
