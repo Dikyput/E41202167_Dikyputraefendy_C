5e1aa1afd4fe5de7e6967a6a8387160d22fcfb00
Diky Putra <joykey.grapichdiky@gmail.com>
2021-03-16 10:28:06 +0700

    Create Tugas3.java

---
+++ Minggu2/Tugas/Tugas3.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugas3;
+
+/**
+ *
+ * @author DIKYPUT PC
+ */
+public class Tugas3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    boolean gameOver = false;
+    int students = 50, classes = 3;
+    double sales_tax;
+    short number1;
+    
+    int 2beOrNot2be;
+        //Pendeklasrasian variable int tidak boleh menggunakan tipe data Karakter dan bilangan bulat
+        //pendeklarasian variabel int seharusnya menggunakan tipe data yang sama atau tidak campur
+    
+        float price index;
+    //Pendeklasrasian variable float tidak boleh menggunakan space
+    //Pendeklasrasian variable float seharusnya menggunkan tipe data bilangan desimal
+    
+    double lastYear'sPrice;
+    //Pendeklasrasian variable double salah dikarenakan menggunkan karakter simbol
+    //Pendeklasrasian variable tidak diperbolehkan menggunkan karakter simbol tertentu
+    
+    long class;
+    //Pendeklarasian diatas salah dikarenkan menggunkan kata kunci yang di dalam program seperti while, class, static dll.
+    //Pendeklarasian Long butuh menggunakan tipe data Karakter yang besar
+    
+    }
+    
+}
