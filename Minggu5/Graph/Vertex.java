a7f02da304590f002b00992a3e57af8cbd44f4d4
Diky Putra <joykey.grapichdiky@gmail.com>
2021-04-12 08:34:08 +0700

    Create Vertex.java

---
+++ Minggu5/Graph/Vertex.java
@@ -0,0 +1,13 @@
+/**
+ *
+ * @author DIKYPUT PC
+ */
+public class Vertex {
+    public char label;
+    public boolean wasVisited;
+    
+    public Vertex(char label) {
+        this.label = label;
+        wasVisited = false;
+    }
+}
